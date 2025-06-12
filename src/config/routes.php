b8d399ee60ef9c3f4f98f43042639aaba1ff569c
honestworker <gonrowskijordan@gmail.com>
2025-06-12 07:40:50 -0500

    Add Controllers and Models

--- src/config/routes.php
+++ src/config/routes.php
@@ -2,17 +2,17 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$route['api/delete/(:any)/(:num)']          = 'PerfexApi/Controllers/$1/data/$2';
-$route['api/(:any)/search/(:any)']          = 'PerfexApi/Controllers/$1/data_search/$2';
-$route['api/(:any)/search']                 = 'PerfexApi/Controllers/$1/data_search';
-$route['api/login/auth']                    = 'PerfexApi/Controllers/login/login_api';
-$route['api/login/view']                    = 'PerfexApi/Controllers/login/view';
-$route['api/login/key']                     = 'PerfexApi/Controllers/login/api_key';
-$route['api/(:any)/(:any)/(:num)']          = 'PerfexApi/Controllers/$1/data/$2/$3';
-$route['api/(:any)/(:num)/(:num)']          = 'PerfexApi/Controllers/$1/data/$2/$3';
-$route['api/custom_fields/(:any)/(:num)']   = 'PerfexApi/Controllers/custom_fields/data/$1/$2';
-$route['api/custom_fields/(:any)']          = 'PerfexApi/Controllers/custom_fields/data/$1';
-$route['api/common/(:any)/(:num)']          = 'PerfexApi/Controllers/common/data/$1/$2';
-$route['api/common/(:any)']                 = 'PerfexApi/Controllers/common/data/$1';
-$route['api/(:any)/(:num)']                 = 'PerfexApi/Controllers/$1/data/$2';
-$route['api/(:any)']                        = 'PerfexApi/Controllers/$1/data';
+$route['api/delete/(:any)/(:num)']          = '$1/data/$2';
+$route['api/(:any)/search/(:any)']          = '$1/data_search/$2';
+$route['api/(:any)/search']                 = '$1/data_search';
+$route['api/login/auth']                    = 'login/login_api';
+$route['api/login/view']                    = 'login/view';
+$route['api/login/key']                     = 'login/api_key';
+$route['api/(:any)/(:any)/(:num)']          = '$1/data/$2/$3';
+$route['api/(:any)/(:num)/(:num)']          = '$1/data/$2/$3';
+$route['api/custom_fields/(:any)/(:num)']   = 'custom_fields/data/$1/$2';
+$route['api/custom_fields/(:any)']          = 'custom_fields/data/$1';
+$route['api/common/(:any)/(:num)']          = 'common/data/$1/$2';
+$route['api/common/(:any)']                 = 'common/data/$1';
+$route['api/(:any)/(:num)']                 = '$1/data/$2';
+$route['api/(:any)']                        = '$1/data';
